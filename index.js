52778538a956d803a8874b7e843de36a873b6ab1
abrusu-collins <snillochemingson@gmail.com>
2021-01-28 23:21:14 -0800

    Navbar resized

--- index.js
+++ index.js
@@ -17,7 +17,13 @@ document.querySelector('.toggler').onclick = () => {
 
 
 
-
+$('[title]').each( function() {
+    var mytitle = $(this);
+    mytitle.data('title',mytitle.attr('title')); // get title attribute value
+mytitle.attr('name', mytitle.attr('title')); // add title attribute value to NAME attribute
+    mytitle.removeAttr('title'); // remove the title attribute, removing browser tooltip
+    
+});
 
 
 
