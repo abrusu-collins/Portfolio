079e67b99b81a1d2ef43fa86b85bc407e498622e
abrusu-collins <snillochemingson@gmail.com>
2022-02-05 15:43:34 -0800

    navbar behaving well now

--- glass.js
+++ glass.js
@@ -89,3 +89,21 @@ contactsbtn.addEventListener("click", function (e) {
   projectsbtn.classList.remove("bcc");
   contactsbtn.classList.add("bcc");
 });
+
+
+//Navbar
+var navbar = document.querySelector('.nav')
+
+window.onscroll = function() {
+
+  // pageYOffset or scrollY
+  if (window.pageYOffset > 600 ) {
+    navbar.classList.add('scrolled')
+  } else {
+    navbar.classList.remove('scrolled')
+  }
+}
+
+document.querySelector('.toggler').onclick = () => {
+  document.querySelector('.nav').classList.toggle('open');
+}
