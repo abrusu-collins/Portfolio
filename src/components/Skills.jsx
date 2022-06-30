500f493aa8a356c1ce0a32f6a3161a9e07a2f863
abrusu-collins <snillochemingson@gmail.com>
2022-06-30 00:03:10 +0000

    Tech stack icons changed

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,63 +1,39 @@
 import "../styles/skills.css";
 import reeact from "./img/react.png";
-import html from "./img/html.jpg";
-import css from "./img/css.webp";
+import html from "./img/html.png";
+import css from "./img/css.png";
 import js from "./img/js.png";
-import git from "./img/git.jpg";
+import git from "./img/git.png";
 import bootstrap from "./img/bootstrap.png";
 import sass from "./img/sass.png";
 import redux from "./img/redux.png";
 
-import AOS from 'aos';
+import AOS from "aos";
 import "aos/dist/aos.css";
 function Skills() {
-    AOS.init();
-    return ( 
+  AOS.init();
+  return (
     <div className="skills" id="skills">
-   <h1>Technical Skills</h1>
-  <div className="techskills">
-    <ul>
-        <li data-aos={"fade-down"}>
-            <img src={html} alt="" />
-            <p>HTML5</p>
-            </li>
-        <li data-aos={"fade-up"}>
-            <img src={css} alt=""   />
-            <p>CSS3</p>
-            </li>
-        <li data-aos={"fade-down"}>
-            <img src={js} alt="" />
-            <p>JavaScript</p>
-            </li>
-        <li data-aos={"fade-up"}>
-            <img src={sass} alt="" />
-            <p>SASS</p>
-            </li>
-        
-        
-   
-    <li data-aos={"fade-down"}>
-        <img src={bootstrap} alt="" />
-        
-        <p>Bootstrap</p>
-        </li>
-        <li data-aos={"fade-up"}>
-        <img src= {reeact}alt="react" />
+      <h1>Technical Skills</h1>
+      <div className="techskills">
+        <img src={html} alt="" data-aos={"fade-down"} />
 
-          <p>  React</p></li>
-        <li data-aos={"fade-down"}>
-            <img src={redux} alt="" />
-           <p> Redux</p>
-            </li>
-        <li data-aos={"fade-up"}>
-            <img src={git} alt="" />
-            <p>Git/GitHub</p>
-            </li>
-    </ul>
-  </div>
+        <img src={css} alt="" data-aos={"fade-up"} />
+
+        <img src={js} alt="" data-aos={"fade-down"} />
+
+        <img src={sass} alt="" data-aos={"fade-up"} />
+
+        <img src={bootstrap} alt="" data-aos={"fade-down"} />
+
+        <img src={reeact} alt="react" data-aos={"fade-up"} />
+
+        <img src={redux} alt="" data-aos={"fade-down"} />
+
+        <img src={git} alt="" data-aos={"fade-up"} />
+      </div>
     </div>
-    
-        );
+  );
 }
 
 export default Skills;
